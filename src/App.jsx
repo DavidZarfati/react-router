70cc728def7b3175ac22d0a81363de35ae00213f
DavidZarfati <zarda2001@gmail.com>
2025-12-09 15:43:23 +0100

    feat:aggiunta di axios,chiamata API, e test prodotti API

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import "bootstrap/dist/css/bootstrap.css"
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import DefaultLayout from './Layouts/DefaulLayout'
 import Home from './pages/Home'
 import Prodotti from './pages/Prodotti'
+import axios from 'axios'
 
 
 function App() {
-
+  const [prodotti, setProdotti] = useState([])
+  function fetchProdotti() {
+    axios.get("https://fakestoreapi.com/products")
+      .then((resp) => {
+        setProdotti(resp.data)
+      })
+  }
+  useEffect(() => {
+    fetchProdotti()
+  }, [])
 
   return (
     <>
-
       <BrowserRouter>
         <Routes>
           <Route element={<DefaultLayout />}>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/chi-siamo' element={<Chi-siamo />}></Route>
-            <Route path='/prodotti' element={<Prodotti />}></Route>
+            <Route path='/' element={<Home />} />
+            <Route path='/chi-siamo' element={<Chi-siamo />} />
+            <Route path='/prodotti' element={<Prodotti prodotti={prodotti} />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -1,8 +1,14 @@
-export default function Prodotti() {
+export default function Prodotti({ prodotti }) {
     return (
         <>
             <h2>benvenuto nei prodotti</h2>
-            <h3>PRODOTTI</h3>
+            {Array.isArray(prodotti) && prodotti.length > 0 ? (
+                prodotti.map((prodotto, index) => (
+                    <h1 key={index}>{prodotto.title}</h1>
+                ))
+            ) : (
+                <p>Nessun prodotto disponibile.</p>
+            )}
         </>
     )
 }
