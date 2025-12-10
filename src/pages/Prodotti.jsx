befe1292ffeaeeaf3371d8c9734e5e9cfab89d0e
DavidZarfati <zarda2001@gmail.com>
2025-12-10 10:15:38 +0100

    aggiunta immagini prodotti

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -5,10 +5,15 @@ export default function Prodotti({ prodotti }) {
             <ul>
                 {Array.isArray(prodotti) && prodotti.length > 0 ? (
                     prodotti.map((prodotto, index) => (
-                        <li key={index}>
-                            <h3>prodotto : {prodotto.title}</h3>
-                            <h5>prezzo : {prodotto.price}$ </h5>
-                            <h6>descrizione : {prodotto.description}</h6>
+                        <li key={index} className="d-flex align-items-center">
+                            <div className="sinistro">
+                                <img src={prodotto.image} alt="" />
+                            </div>
+                            <div className="destro p-2">
+                                <h3>prodotto : {prodotto.title}</h3>
+                                <h5>prezzo : {prodotto.price}$ </h5>
+                                <h6>descrizione : {prodotto.description}</h6>
+                            </div>
                         </li>
                     ))
                 ) : (
