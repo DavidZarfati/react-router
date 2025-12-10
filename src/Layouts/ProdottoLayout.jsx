d7f62ad7261376eec684a2557760a4e5f25af83e
DavidZarfati <zarda2001@gmail.com>
2025-12-10 11:57:08 +0100

    feat:aggiunta possibilita di tornare a sezione prodotti

--- src/Layouts/ProdottoLayout.jsx
+++ src/Layouts/ProdottoLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 export default function ProdottoLayout() {
     const { id } = useParams()
     // console.log(id);
@@ -8,6 +9,13 @@ export default function ProdottoLayout() {
         <>
             <section>
                 <h1>Pagina del singolo prodotto {id}</h1>
+                <NavLink to={`/`} >
+                    Torna alla Home
+                </NavLink>
+                <br />
+                <NavLink to={`/prodotti`} >
+                    Torna alla sezione prodotti
+                </NavLink>
             </section>
         </>
     )
